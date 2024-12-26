64e03c61b43826638c442efb9fcb475525e6692f
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-12-26 15:02:29 +0330

    Part of the Executor was written (arithmetic and boolean ops)

--- source/stackvm.d
+++ source/stackvm.d
@@ -13,7 +13,8 @@ alias DataStack = TValue[MAX_DATA];
 alias CallStack = CallFrame[MAX_CALL];
 alias CodeList = Instruction[MAX_CODE];
 alias ConstantPool = TValue[MAX_CONST];
-alias Address = size_t;
+alias Address = long;
+alias Index = ubyte;
 alias TableEntries = SList!Entry;
 
 class Table
@@ -117,9 +118,95 @@ struct Entry
     }
 }
 
+struct Closure
+{
+    Address program_counter;
+    CallFrame frame;
+    private ConstantPool constants;
+
+    this(Address program_counter, CallFrame frame)
+    {
+        this.program_counter = program_counter;
+        this.frame = frame;
+        this.constants = null;
+    }
+
+    TValue getConstant(Index index)
+    {
+        if (index >= CONST_MAX)
+            throw new StackVMError("Constant index too high", index);
+        return this.constants[index];
+    }
+
+    void setConstant(Index index, TValue value)
+    {
+        if (index >= CONST_MAX)
+            throw new StackVMError("Constant index too high", index);
+        this.constants[index] = value;
+    }
+
+    bool opEquals(const Closure rhs) const
+    {
+        return this.code == rhs.code && this.frame == rhs.frame;
+    }
+}
+
 enum Instruction
 {
     Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Pow,
+    Shr,
+    Shl,
+    Neg,
+    Eq,
+    Ne,
+    Gt,
+    Ge,
+    Lt,
+    Le,
+    Conj,
+    Disj,
+    Not,
+    BitAnd,
+    BitNot,
+    BitXor,
+    BitOr,
+    LoadNil,
+    LoadBoolean,
+    LoadString,
+    LoadNumber,
+    LoadAddress,
+    LoadTable,
+    LoadIndex,
+    StoreNil,
+    StoreBoolean,
+    StoreString,
+    StoreNumber,
+    StoreAddress,
+    StoreTable,
+    StoreIndex,
+    GetTableEntry,
+    SetTableEntry,
+    SetConstAtTopFrame,
+    GetConstAtTopFrame,
+    SetConstAtGlobals,
+    GetConstAtGlobals,
+    DuplicateTop,
+    SwapTop,
+    RotateTop3,
+    RotateTop4,
+    Jump,
+    JumpIfTrue,
+    JumpIfFalse,
+    ReturnFromFunction,
+    Varargs,
+    MarkClosure,
+    CallClosure,
+    CallFunction,
 }
 
 class StackVMError : Exception
@@ -139,27 +226,37 @@ struct TValue
     enum TValueKind
     {
         Nil,
-        True,
-        False,
+        Boolean,
         String,
         Number,
         Address,
         Table,
+        Index,
+        Closure,
     }
 
     TValueKind kind;
 
     union
     {
+        bool v_boolean;
         string v_string;
         real v_number;
         Address v_address;
         Table v_table;
+        Index v_index;
+        Closure v_closure;
     }
 
     this(TValueKind kind)
     {
         this.kind = kind;
+    }
+
+    this(bool v_boolean)
+    {
+        this.kind = TValueKind.Boolean;
+        this.v_boolean = v_boolean;
     }
 
     this(string v_string)
@@ -186,19 +283,26 @@ struct TValue
         this.v_table = v_table;
     }
 
+    this(Index v_index)
+    {
+        this.kind = TValueKind.Index;
+        this.v_index = v_index;
+    }
+
+    this(Closure v_closure)
+    {
+        this.kind = TValueKind.Closure;
+        this.v_closure = v_closure;
+    }
+
     static TValue newNil()
     {
         return TValue(TValueKind.Nil);
     }
 
-    static TValue newTrue()
+    static TValue newBoolean(bool v_boolean)
     {
-        return TValue(TValueKind.True);
-    }
-
-    static TValue newFalse()
-    {
-        return TValue(TValueKind.False);
+        return TValue(v_boolean);
     }
 
     static TValue newString(string v_string)
@@ -216,6 +320,21 @@ struct TValue
         return TValue(v_address);
     }
 
+    static TValue newTable(Table v_table)
+    {
+        return TValue(v_table);
+    }
+
+    static TValue newIndex(Index v_index)
+    {
+        return TValue(v_index);
+    }
+
+    static TValue newClosure(Closure v_closure)
+    {
+        return TValue(v_closure);
+    }
+
     bool opEquals(const TValue rhs) const
     {
         if (this.kind == rhs.kind)
@@ -223,9 +342,9 @@ struct TValue
             switch (this.kind)
             {
             case TValueKind.Nil:
-            case TValueKind.True:
-            case TValueKind.False:
                 return true;
+            case TValueKind.Boolean:
+                return this.v_boolean == rhs.v_boolean;
             case TValueKind.String:
                 return this.v_string == rhs.v_string;
             case TValueKind.Number:
@@ -234,6 +353,10 @@ struct TValue
                 return this.v_address == rhs.v_address;
             case TValueKind.Table:
                 return this.v_table == rhs.v_table;
+            case TValueKind.Index:
+                return this.v_index == rhs.v_index;
+            case TValueKind.Closure:
+                return this.v_closure == rhs.v_closure;
             default:
                 return false;
             }
@@ -246,10 +369,10 @@ struct CallFrame
 {
     Address return_address;
     Address static_link;
-    size_t nargs, nlocals;
+    Index nargs, nlocals;
     private ConstantPool constants;
 
-    this(Address return_address, Address static_link, size_t nargs, size_t nlocals)
+    this(Address return_address, Address static_link, Index nargs, Index nlocals)
     {
         this.return_address = return_address;
         this.static_link = static_link;
@@ -258,14 +381,14 @@ struct CallFrame
         this.constants = null;
     }
 
-    void setConstant(size_t index, TValue constant)
+    void setConstant(Index index, TValue constant)
     {
         if (index >= MAX_CONST)
             throw new StackVMError("Constant index too high", this.return_address);
         this.constants[index] = constant;
     }
 
-    TValue getConstant(size_t index)
+    TValue getConstant(Index index)
     {
         if (index >= MAX_CONST)
             throw new StackVMError("Constant index too high", this.return_address);
@@ -283,17 +406,19 @@ class Interpreter
     private Address program_counter;
     private size_t code_size;
     private size_t call_size;
+    private ConstantPool globals;
 
     this()
     {
         this.data_stack = null;
         this.call_stack = null;
         this.code_list = null;
-        this.stack_pointer = 0;
-        this.frame_pointer = 0;
+        this.stack_pointer = -1;
+        this.frame_pointer = -1;
         this.program_counter = 0;
-        this.code_size = 0;
-        this.call_size = 0;
+        this.code_size = -1;
+        this.call_size = -1;
+        this.globals = null;
     }
 
     void pushData(TValue value)
@@ -308,14 +433,25 @@ class Interpreter
         pushData(TValue.newNil());
     }
 
-    void pushTrue()
+    bool popNil()
     {
-        pushData(TValue.newTrue());
+        auto data = popData();
+        if (data.kind != TValue.TValueKind.Nil)
+            throw new StackVMError("Expected Nil value at TOS", this.stack_pointer);
+        return true;
     }
 
-    void pushFalse()
+    void pushBoolean(bool value)
     {
-        pushData(TValue.newFalse());
+        pushData(TValue.newBoolean(value));
+    }
+
+    bool popBoolean()
+    {
+        auto data = popData();
+        if (data.kind != TValue.TValueKind.Boolean)
+            throw new StackVMError("Expected Boolean value at TOS", this.stack_pointer);
+        return data.v_boolean;
     }
 
     void pushString(string value)
@@ -323,9 +459,25 @@ class Interpreter
         pushData(TValue.newString(value));
     }
 
+    string popString()
+    {
+        auto data = popData();
+        if (data.kind != TValue.TValueKind.Boolean)
+            throw new StackVMError("Expected String value at TOS", this.stack_pointer);
+        return data.v_string;
+    }
+
     void pushNumber(real value)
     {
         pushData(TValue.newNumber(value));
+    }
+
+    real popNumber()
+    {
+        auto data = popData();
+        if (data.kind != TValue.TValueKind.Number)
+            throw new StackVMError("Expected Number value at TOS", this.stack_pointer);
+        return data.v_number;
     }
 
     void pushAddress(Address value)
@@ -333,55 +485,181 @@ class Interpreter
         pushData(TValue.newAddress(value));
     }
 
+    Address popAddress()
+    {
+        auto data = popData();
+        if (data.kind != TValue.TValueKind.Address)
+            throw new StackVMError("Expected Address value at TOS", this.stack_pointer);
+        return data.v_address;
+    }
+
+    void pushTable(Table value)
+    {
+        pushData(TValue.newTable(value));
+    }
+
+    Table popTable()
+    {
+        auto data = popData();
+        if (data.kind != TValue.TValueKind.Table)
+            throw new StackVMError("Expected Table value at TOS", this.stack_pointer);
+        return data.v_table;
+    }
+
+    void pushIndex(Index value)
+    {
+        pushData(TValue.newIndex(value));
+    }
+
+    Index popIndex()
+    {
+        auto data = popData();
+        if (data.kind != TValue.TValueKind.Index)
+            throw new StackVMError("Expected Index value at TOS", this.stack_pointer);
+        return data.v_index;
+    }
+
+    void pushClosure(Closure value)
+    {
+        pushData(TValue.newClosure(value));
+    }
+
+    Closure popClosure()
+    {
+        auto data = popData();
+        if (data.kind != TValue.TValueKind.Closure)
+            throw new StackVMError("Expected Closure value at TOS", this.stack_pointer);
+        return data.v_closure;
+    }
+
+    void swapTopOfDataStack()
+    {
+        if (this.stack_pointer < 2)
+            throw new StackVMError("Not enough data for swap", this.stack_pointer);
+        auto tmp = topData();
+        this.data_stack[this.stack_pointer] = this.data_stack[this.stack_pointer - 1];
+        this.data_stack[this.stack_pointer - 1] = tmp;
+    }
+
+    void duplicateTopOfDataStack()
+    {
+        if (this.stack_pointer <= 0)
+            throw new StackVMError("Not enough data for duplicate", this.stack_pointer);
+        auto top = topDate();
+        pushDate(top);
+    }
+
+    void rotateTop3OfDataStack()
+    {
+        if (this.stack_pointer < 3)
+            throw new StackVMError("Not enough data for Rot3", this.stack_pointer);
+        auto tmp1 = topData();
+        auto tmp2 = this.data_stack[this.stack_pointer - 1];
+        auto tmp3 = this.data_stack[this.stack_pointer - 2];
+        this.data_stack[this.stack_pointer] = tmp3;
+        this.data_stack[this.stack_pointer - 1] = tmp2;
+        this.data_stack[this.stack_pointer - 2] = tmp1;
+    }
+
+    void rotateTop4OfDataStack()
+    {
+        if (this.stack_pointer < 4)
+            throw new StackVMError("Not enough data for Rot4", this.stack_pointer);
+        auto tmp1 = topData();
+        auto tmp2 = this.data_stack[this.stack_pointer - 1];
+        auto tmp3 = this.data_stack[this.stack_pointer - 2];
+        auto tmp4 = this.data_stack[this.stack_pointer - 3];
+        this.data_stack[this.stack_pointer] = tmp4;
+        this.data_stack[this.stack_pointer - 1] = tmp3;
+        this.data_stack[this.stack_pointer - 2] = tmp2;
+        this.data_stack[this.stack_pointer - 3] = tmp1;
+    }
+
     TValue popData()
     {
-        if (this.stack_pointer == 0)
+        if (this.stack_pointer <= 0)
             throw new StackVMError("Data stack underflow", this.stack_pointer);
         return this.data_stack[this.stack_pointer--];
     }
 
-    TValue getArgument(size_t number)
+    TValue topData()
     {
-        auto call_frame = this.call_stack[this.call_size - 1];
-        auto nargs = call_frame.nargs;
-        return this.data_stack[this.frame_pointer + (nargs - number)];
+        return this.data_stack[this.stack_pointer];
     }
 
-    TValue getLocal(size_t number)
+    TValue getArgument(Index index)
     {
-        auto call_frame = this.call_stack[this.call_size - 1];
+        auto call_frame = topCallFrame();
+        auto nargs = call_frame.nargs;
+        return this.data_stack[this.frame_pointer + (nargs - index)];
+    }
+
+    TValue getLocal(Index index)
+    {
+        auto call_frame = topCallFrame();
         auto nlocals = call_frame.nlocals;
         auto nargs = call_frame.nargs;
-        return this.data_stack[this.frame_pointer + ((nargs + nlocals) - number)];
+        return this.data_stack[this.frame_pointer + ((nargs + nlocals) - index)];
     }
 
-    TValue getConstantAtTopFrame(size_t index)
+    TValue getConstantAtGlobals(Index index)
     {
-        auto call_frame = this.call_stack[this.call_size - 1];
+        if (index >= MAX_CONST)
+            throw new StackVMError("Global index too high", index);
+        return this.globals[index];
+    }
+
+    void setConstantAtGlobals(Index index, TValue value)
+    {
+        if (index >= MAX_CONST)
+            throw new StackVMError("Global index too high", index);
+        this.globals[index] = value;
+    }
+
+    TValue getConstantAtTopFrame(Index index)
+    {
+        auto call_frame = getTopFrame();
         return call_frame.getConstant(index);
     }
 
-    void setConstantAtTopFrame(size_t index, TValue value)
+    CallFrame getTopFrame() const
     {
-        auto call_frame = this.call_stack[this.call_size - 1];
+        return this.call_stack[this.call_size];
+    }
+
+    CallFrame popCallFrame()
+    {
+        if (this.call_size <= 0)
+            throw new StackVMError("Call stack underflow", this.call_size);
+        return this.call_stack[this.call_size--];
+    }
+
+    void setConstantAtTopFrame(Index index, TValue value)
+    {
+        auto call_frame = getTopFrame();
         call_frame.setConstant(index, value);
     }
 
     void insertCode(Instruction code)
     {
-        this.code_list[this.code_size++] = code;
+        this.code_list[++this.code_size] = code;
     }
 
     Instruction nextCode()
     {
-        if (this.code_size + 1 < this.program_counter)
+        if (this.code_size <= this.program_counter)
             throw new StackVMError("Code list overflow", this.program_counter);
         return this.code_list[this.program_counter++];
     }
 
-    void setupNewCallFrame(size_t nargs, size_t nlocals)
+    bool codeRemains()
     {
-        if (this.call_size + 1 >= MAX_CALL)
+        return this.program_counter < this.code_size;
+    }
+
+    void setupNewCallFrame(Index nargs, Index nlocals)
+    {
+        if (this.call_size >= MAX_CALL)
             throw new StackVMError("Call stack overflow", this.call_size);
         this.call_stack[++this.call_size] = CallFrame(this.stack_pointer + nargs + nlocals,
                 this.frame_pointer, nargs, nlocals);
@@ -390,9 +668,9 @@ class Interpreter
 
     void popCallStackAndClear()
     {
-        if (this.call_size == 0)
+        if (this.call_size <= 0)
             throw new StackVMError("Call stack underflow", this.call_size);
-        auto call_frame = this.call_stack[this.call_size--];
+        auto call_frame = popCallFrame();
         auto return_address = call_frame.return_address;
         auto static_link = call_frame.static_link;
         auto nargs = call_frame.nargs;
@@ -409,5 +687,79 @@ class Interpreter
     Address getProgramCounter()
     {
         return this.program_counter;
+    }
+
+    void runVM()
+    {
+        while (codeRemains())
+        {
+            Instruction next_code = nextCode();
+
+            switch (next_code)
+            {
+            case Instruction.Add:
+		auto rhs = popNumber();
+		auto lhs = popNumber();
+		pushNumber(lhs + rhs);
+	    case Instruction.Sub:
+		auto rhs = popNumber();
+		auto lhs = popNumber();
+		pushNumber(lhs - rhs);
+	    case Instruction.Mul:
+		auto rhs = popNumber();
+		auto lhs = popNumber();
+		pushNumber(lhs * rhs);
+	    case Instruction.Div:
+		auto rhs = popNumber();
+		auto lhs = popNumber();
+		pushNumber(lhs / rhs);
+	    case Instruction.Mod:
+		ulong rhs = cast(ulong) popNumber();
+		ulong lhs = cast(ulong) popNumber();
+		pushNumber(lhs % rhs);
+	    case Instruction.Pow:
+		auto rhs = popNumber();
+		auto lhs = popNumber();
+		pushNumber(lhs ^^ rhs);
+	    case Instruction.Shr:
+		ulong rhs = cast(ulong) popNumber();
+		ulong lhs = cast(ulong) popNumber();
+		pushNumber(lhs >> rhs);
+	    case Instruction.Shl:
+		ulong rhs = cast(ulong) popNumber();
+		ulong lhs = cast(ulong) popNumber();
+		pushNumber(lhs << rhs);
+	    case Instruction.Neg:
+		auto num = popNumber();
+		pushNumber(-num);
+	    case Instruction.Conj:
+		auto rhs = popBoolean();
+		auto lhs = popBoolean();
+		pushBoolean(lhs && rhs);
+	    case Instruction.Disj:
+		auto rhs = popBoolean();
+		auto lhs = popBoolean();
+		pushBoolean(lhs || rhs);
+	    case Instruction.Not:
+		auto boolean = popBoolean();
+		pushBoolean(!boolean);
+	    case Instruction.BitAnd:
+		ulong rhs = cast(ulong) popNumber();
+		ulong lhs = cast(ulong) popNumber();
+		pushNumber(lhs & rhs);
+	    case Instruction.BitOr:
+		ulong rhs = cast(ulong) popNumber();
+		ulong lhs = cast(ulong) popNumber();
+		pushNumber(lhs | rhs);
+	    case Instruction.BitXor:
+		ulong rhs = cast(ulong) popNumber();
+		ulong lhs = cast(ulong) popNumber();
+		pushNumber(lhs ^ rhs);
+	    case Instruction.BitNot:
+		ulong num = cast(ulong) popNumber();
+		pushNumber(~num);
+            }
+
+        }
     }
 }
